0e3e1758284adf4239b4b2cffcfb8d569edb7d36
Jani Monoses <jani@ubuntu.com>
2014-05-06 12:52:41 +0300

    Add tool to generate GPG keys and tarballs
    Signed-off-by: Jani Monoses <jani@ubuntu.com>

---
+++ generate_keys.go
@@ -0,0 +1,241 @@
+// +build ignore
+
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"code.google.com/p/go.crypto/openpgp"
+	"github.com/remyoudompheng/go-liblzma"
+)
+
+const (
+	secretKeyDir = "keys/gpg"
+	wwwKeyDir    = "www/gpg"
+)
+
+type KeyringJson struct {
+	Expiry int64  `json:"expiry,omitempty"`
+	Type   string `json:"type"`
+}
+
+// Key describes a key used in signing image tarballs
+type Key struct {
+	typ     string
+	dir     string
+	name    string
+	comment string
+	email   string
+	signer  string
+	expiry  *time.Time
+	entity  *openpgp.Entity
+}
+
+// signFile creates a GPG detached ASCII armored signature (.asc file) for a given file
+func signFile(path string, ent *openpgp.Entity) {
+	sigpath := path + ".asc"
+	reader, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	writer, err := os.Create(sigpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	openpgp.ArmoredDetachSign(writer, ent, reader, nil)
+}
+
+// generate creates a GPG keypair and keyring tarballs
+func (k *Key) generate() error {
+	ent, err := openpgp.NewEntity(k.name, k.comment, k.email, nil)
+	if err != nil {
+		return err
+	}
+
+	//FIXME: how to not have subkeys otherwise?
+	ent.Subkeys = nil
+
+	k.entity = ent
+
+	os.MkdirAll(k.dir, 0700)
+
+	pubring, err := os.Create(filepath.Join(k.dir, "pubring.gpg"))
+	if err != nil {
+		return err
+	}
+
+	secring, err := os.Create(filepath.Join(k.dir, "secring.gpg"))
+	if err != nil {
+		return err
+	}
+
+	if err = ent.SerializePrivate(secring, nil); err != nil {
+		return err
+	}
+
+	if err = ent.Serialize(pubring); err != nil {
+		return err
+	}
+	k.writeKeyringJson()
+	return nil
+}
+
+func (k *Key) writeKeyringJson() error {
+	kr := &KeyringJson{Type: k.typ}
+	if k.expiry != nil {
+		kr.Expiry = k.expiry.Unix()
+	}
+	b, err := json.MarshalIndent(kr, "", "    ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filepath.Join(k.dir, "keyring.json"), b, 0600)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+// Default expiry date for image-signing and device-signing keys
+var twoYearsFromNow = time.Now().AddDate(2, 0, 0)
+
+var signingKeys = map[string]*Key{
+	"archive-master": {
+		name:   "[TESTING] Ubuntu Archive Master Signing Key",
+		email:  "ftpmaster@ubuntu.com",
+		expiry: nil,
+		signer: "archive-master",
+	},
+	"image-master": {
+		name:   "[TESTING] Ubuntu System Image Master Signing Key",
+		email:  "system-image@ubuntu.com",
+		expiry: nil,
+		signer: "archive-master",
+	},
+	"image-signing": {
+		name:    "[TESTING] Ubuntu System Image Signing Key",
+		comment: "YYYY",
+		email:   "system-image@ubuntu.com",
+		expiry:  &twoYearsFromNow,
+		signer:  "image-master",
+	},
+	"device-signing": {
+		name:    "[TESTING] Random OEM Signing Key",
+		comment: "YYYY",
+		email:   "system-image@ubuntu.com",
+		expiry:  &twoYearsFromNow,
+		signer:  "image-signing",
+	},
+}
+
+func createGPGKeys() error {
+	for n, k := range signingKeys {
+		log.Printf("Creating %s GPG key\n", n)
+		k.typ = n
+		k.dir = filepath.Join(secretKeyDir, n)
+		err := k.generate()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//tarDirectory makes a tar.xz archive from the given directory
+func tarDirectory(dir, name string) error {
+	var buf bytes.Buffer
+	tw := tar.NewWriter(&buf)
+	defer tw.Close()
+
+	if err := addFile(filepath.Join(dir, "keyring.json"), "keyring.json", tw); err != nil {
+		return err
+	}
+	if err := addFile(filepath.Join(dir, "pubring.gpg"), "keyring.gpg", tw); err != nil {
+		return err
+	}
+
+	tw.Close()
+	tarxz, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer tarxz.Close()
+
+	xzw, err := xz.NewWriter(tarxz, xz.LevelDefault)
+	if err != nil {
+		return err
+	}
+	defer xzw.Close()
+
+	_, err = io.Copy(xzw, &buf)
+	return nil
+}
+
+//addFile adds the file named name as a tar entry named tarname
+func addFile(name string, tarname string, tw *tar.Writer) error {
+	if tarname == "" {
+		tarname = name
+	}
+	fi, err := os.Stat(name)
+	if err != nil {
+		return err
+	}
+	hdr, err := tar.FileInfoHeader(fi, "")
+	if err != nil {
+		return err
+	}
+	hdr.Name = tarname
+
+	err = tw.WriteHeader(hdr)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(tw, f)
+	if err != nil {
+		return err
+	}
+	tw.Flush()
+	return nil
+}
+
+func createKeyrings() error {
+	for d, k := range signingKeys {
+		p := filepath.Join(wwwKeyDir, d) + ".tar.xz"
+		log.Printf("Creating %s keyring tarball\n", d)
+		if err := tarDirectory(k.dir, p); err != nil {
+			return err
+		}
+		signFile(p, signingKeys[k.signer].entity)
+	}
+	return nil
+}
+
+func init() {
+	os.MkdirAll(wwwKeyDir, 0700)
+}
+
+func main() {
+	log.SetFlags(0)
+	err := createGPGKeys()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = createKeyrings()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
