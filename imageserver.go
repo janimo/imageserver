e7c52b61e87710c7a31e71c2b1ba638eefd4af05
Jani Monoses <jani@ubuntu.com>
2014-07-30 14:32:20 +0300

    Add licensing headers
    Signed-off-by: Jani Monoses <jani@ubuntu.com>

--- imageserver.go
+++ imageserver.go
@@ -1,4 +1,23 @@
+//
+// imageserver - Server for Ubuntu Touch system images
+//
+// Copyright (c) 2014 Canonical Ltd.
+//
+// Author: Jani Monoses <jani.monoses@canonical.com>
+
 package main
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License version 3, as published
+// by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranties of
+// MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+// PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import (
 	"crypto/sha256"
